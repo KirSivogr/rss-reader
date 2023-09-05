af653f4ee7eee56e708978e0db96692edde25658
sivog <sivograkovkirill@mail.ru>
2023-09-05 22:58:24 +0300

    add i18

---
+++ src/application/utils/i18init.js
@@ -0,0 +1,21 @@
+import i18next from "i18next";
+
+const i18nextInstance = i18next.createInstance();
+
+i18nextInstance.init({
+    fallbackLng: 'ru',
+    debug: true,
+    resources: {
+        ru: {
+            translation: {
+                successAnswer: 'RSS успешно загружен',
+                validationError: 'Ссылка должна быть валидным URL',
+                existenceError: 'RSS уже существует',
+                feeds: 'Фиды',
+                posts: 'Посты'
+            }
+        }
+    }
+})
+
+export default i18nextInstance
