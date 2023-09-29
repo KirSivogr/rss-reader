57120301f2f3cc495e3aff1bb8f8eb534968e370
sivog <sivograkovkirill@mail.ru>
2023-09-29 21:17:28 +0300

    fix render functions

--- src/application/utils/renderFeeds.js
+++ src/application/utils/renderFeeds.js
@@ -1,27 +1,26 @@
-import createTitleAndUl from "./createTitleAndUl.js";
+import createTitleAndUl from './createTitleAndUl.js';
 
 const createNewFeed = (title, description, feedUl) => {
-    const li = document.createElement('li');
-    const h3 = document.createElement('h3');
-    const p = document.createElement('p');
+  const li = document.createElement('li');
+  const h3 = document.createElement('h3');
+  const p = document.createElement('p');
 
-    p.classList.add('m-0', 'small', 'text-black-50');
-    p.innerHTML = description;
-    h3.classList.add('h6', 'm-0');
-    h3.innerHTML = title;
-    li.classList.add('list-group-item', 'border-0', 'border-end-0');
+  p.classList.add('m-0', 'small', 'text-black-50');
+  p.innerHTML = description;
+  h3.classList.add('h6', 'm-0');
+  h3.innerHTML = title;
+  li.classList.add('list-group-item', 'border-0', 'border-end-0');
 
-    li.append(h3);
-    li.append(p);
-    feedUl.append(li);
-}
+  li.append(h3);
+  li.append(p);
+  feedUl.prepend(li);
+};
 
 export default (elements, value, prevValue) => {
-    console.log(prevValue.length);
-    if (prevValue.length === 0) {
-        createTitleAndUl(elements.feeds, 'Фиды');
-    }
+  if (prevValue.length === 0) {
+    createTitleAndUl(elements.feeds, 'Фиды');
+  }
 
-    const feedUl = elements.feeds.querySelector('ul');
-    createNewFeed(value[value.length - 1].feedTitle, value[value.length - 1].feedDescription, feedUl);
-}
+  const feedUl = elements.feeds.querySelector('ul');
+  createNewFeed(value[0].feedTitle, value[0].feedDescription, feedUl);
+};
--- src/application/utils/renderPosts.js
+++ src/application/utils/renderPosts.js
@@ -1,36 +1,61 @@
-import createTitleAndUl from "./createTitleAndUl.js";
+import createTitleAndUl from './createTitleAndUl.js';
 
-const createNewPosts = (link, title, postsUl) => {
-    const li = document.createElement('li');
-    const a = document.createElement('a');
-    const button = document.createElement('button');
+const createNewPosts = (id, link, title, description, postsUl, state) => {
+  const li = document.createElement('li');
+  const a = document.createElement('a');
+  const button = document.createElement('button');
 
-    button.classList.add('btn', 'btn-outline-primary', 'btn-sm');
-    button.setAttribute('type', 'button');
-    button.textContent = 'Просмотр';
-    button.dataset.bsToggle = 'modal';
-    button.dataset.bsTarget = '#exampleModal';
-    a.href = link;
-    a.classList.add('fw-bold');
-    a.textContent = title;
-    a.setAttribute('target', '_blank');
-    a.setAttribute('rel', 'noopener noreferrer');
-    li.classList.add('list-group-item', 'd-flex', 'justify-content-between', 'align-items-start', 'border-0', 'border-end-0');
+  button.classList.add('btn', 'btn-outline-primary', 'btn-sm');
+  button.setAttribute('type', 'button');
+  button.textContent = 'Просмотр';
+  button.dataset.bsToggle = 'modal';
+  button.dataset.bsTarget = '#exampleModal';
+  button.dataset.id = id;
 
+  a.dataset.id = id;
+  a.href = link;
+  a.classList.add('fw-bold');
+  a.textContent = title;
+  a.setAttribute('target', '_blank');
+  a.setAttribute('rel', 'noopener noreferrer');
 
-    li.append(a);
-    li.append(button);
-    postsUl.append(li);
-}
+  a.addEventListener('click', () => {
+    a.classList.remove('fw-bold');
+    a.classList.add('fw-normal', 'link-secondary');
+    state.readPosts.push({
+      id, link, title, description,
+    });
+  });
 
-export default (elements, value, prevValue) => {
-    if (prevValue.length === 0) {
-        createTitleAndUl(elements.posts, 'Посты');
-    }
+  button.addEventListener('click', () => {
+    const modalTitle = document.querySelector('.modal-title');
+    const modalBody = document.querySelector('.modal-body');
+    const article = document.querySelector('.full-article');
+    modalTitle.textContent = title;
+    modalBody.classList.add('text-break');
+    modalBody.textContent = description;
+    article.href = link;
+    a.classList.remove('fw-bold');
+    a.classList.add('fw-normal', 'link-secondary');
+    state.readPosts.push({
+      id, link, title, description,
+    });
+  });
 
-    const postsUl = elements.posts.querySelector('ul');
-    value = value.slice(prevValue.length);
-    value.forEach((post) => {
-        createNewPosts(post.link, post.title, postsUl);
-    })
-}
+  li.classList.add('list-group-item', 'd-flex', 'justify-content-between', 'align-items-start', 'border-0', 'border-end-0');
+  li.append(a);
+  li.append(button);
+  postsUl.prepend(li);
+};
+
+export default (elements, value, prevValue, state) => {
+  if (prevValue.length === 0) {
+    createTitleAndUl(elements.posts, 'Посты');
+  }
+
+  const postsUl = elements.posts.querySelector('ul');
+  value = value.slice(0, value.length - prevValue.length).reverse();
+  value.forEach((post) => {
+    createNewPosts(post.id, post.link, post.title, post.description, postsUl, state);
+  });
+};
