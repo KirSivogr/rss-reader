f967ba6a0040afa30d97754d2d5ccceaac24845d
sivog <sivograkovkirill@mail.ru>
2023-09-29 21:19:39 +0300

    upgrade parser

--- src/application/utils/parser.js
+++ src/application/utils/parser.js
@@ -1,21 +1,29 @@
-export default (content) => {
+import _ from 'lodash';
+
+export default (content, feedId = 0) => {
+  try {
     const parser = new DOMParser();
     const doc = parser.parseFromString(content, 'text/xml');
-    const feedTitle  = doc.querySelector('title').textContent;
+    const feedTitle = doc.querySelector('title').textContent;
     const feedDescription = doc.querySelector('description').textContent;
     const postsArray = Array.from(doc.querySelectorAll('item'));
-    const posts = postsArray.map((item) => {
-        return {
-            title: item.querySelector('title').textContent,
-            link: item.querySelector('link').textContent
-        }
-    });
+    const posts = postsArray.map((item) => ({
+      feedId,
+      id: _.uniqueId(),
+      title: item.querySelector('title').textContent,
+      link: item.querySelector('link').textContent,
+      description: item.querySelector('description').textContent,
+    }));
     const feed = {
-        feedTitle,
-        feedDescription
-    }
+      feedId,
+      feedTitle,
+      feedDescription,
+    };
     return {
-        feed,
-        posts
-    }
-}
+      feed,
+      posts,
+    };
+  } catch (e) {
+    throw TypeError;
+  }
+};
