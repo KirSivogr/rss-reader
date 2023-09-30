47a33f7c4656d945f64484893f1e18021b2981c6
sivog <sivograkovkirill@mail.ru>
2023-09-30 14:21:59 +0300

    add some errors

--- src/application/utils/i18init.js
+++ src/application/utils/i18init.js
@@ -1,21 +1,23 @@
-import i18next from "i18next";
+import i18next from 'i18next';
 
 const i18nextInstance = i18next.createInstance();
 
 i18nextInstance.init({
-    fallbackLng: 'ru',
-    debug: true,
-    resources: {
-        ru: {
-            translation: {
-                successAnswer: 'RSS успешно загружен',
-                validationError: 'Ссылка должна быть валидным URL',
-                existenceError: 'RSS уже существует',
-                feeds: 'Фиды',
-                posts: 'Посты',
-            }
-        }
-    }
-})
+  fallbackLng: 'ru',
+  debug: true,
+  resources: {
+    ru: {
+      translation: {
+        successAnswer: 'RSS успешно загружен',
+        validationError: 'Ссылка должна быть валидным URL',
+        existenceError: 'RSS уже существует',
+        axiosErrorAnswer: 'Ошибка сети',
+        typeErrorAnswer: 'Ресурс не содержит валидный RSS',
+        feeds: 'Фиды',
+        posts: 'Посты',
+      },
+    },
+  },
+});
 
-export default i18nextInstance
+export default i18nextInstance;
